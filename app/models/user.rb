8dd49da464de170de5549547d87cbb1a3188a51a
Cindy Van Dooren <cindy.vandooren@gmail.com>
2015-07-18 13:52:22 -0700

    Add length validation on user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,7 @@ class User < ActiveRecord::Base
 
   validates :username, :email, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
+  validates :email, length: { minimum: 10 }
   after_initialize  :ensure_session_token
 
   attr_reader :password
