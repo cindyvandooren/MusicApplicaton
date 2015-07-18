958a9bf7cdc7e81101efe8a874e2964a79a8b51c
Cindy Van Dooren <cindy.vandooren@gmail.com>
2015-07-18 15:45:22 -0700

    Added routes for albums and tracks

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
+  resources :bands do
+    resources :albums, only: [:new]
+  end
 
-  resources :bands
+  resources :albums, except: [:new, :index] do
+    resources :tracks, only: [:new]
+  end
+
+  resources :tracks, except: [:new, :index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
