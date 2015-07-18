bbe3e2db9b8e7c92114945885232dd06e8f92c93
Cindy Van Dooren <cindy.vandooren@gmail.com>
2015-07-18 14:11:34 -0700

    Add Band model

---
+++ app/models/band.rb
@@ -0,0 +1,4 @@
+class Band < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150718192544) do
+ActiveRecord::Schema.define(version: 20150718210520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bands", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "bands", ["name"], name: "index_bands_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
